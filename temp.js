061f9e5bc2adafa3ad2a5bba23762bcb138c0fd1
ali <wizard2070@gmail.com>
2021-06-03 22:49:18 +0430

    Added Pagination

--- temp.js
+++ temp.js
@@ -1,9 +1,10 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const mysql = require('mysql');
+const chalk = require('chalk');
 const config = require('./config');
-const url = "https://www.daneshjooyar.com";
-
+const url = "https://www.digikala.com/search/category-mobile-phone";
+const cat_id = 1;
 
 const connection = mysql.createConnection({
     host: config.db.fullstack_express.host,
@@ -13,49 +14,65 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
+let pageLimit = 3;
+let pageCounter = 0;
+let parsedResults = [];
+console.log(chalk.yellow.bgBlue(`\n  Scraping of ${chalk.underline.bold(url)} initiated...\n`))
 
-fetchData(url).then( (res) => {
-    const html = res.data;
-    const $ = cheerio.load(html);
-    const selector = $('.container.section.autocv > .row >  .col-xl-3.col-lg-4.col-sm-6.col-xs-12');
+const fetchData = async (url) => {
+    try {
+        const html = await axios.get(url);
+        const $ = cheerio.load(html.data);
+        const selector = $('ul.c-listing__items.js-plp-products-list > li > .c-product-box');
 
-    let arr = [];
-    let obj = {};
-    selector.map(function (index) {
-        let href = $(this).find('a').attr('href');
-        let title = $(this).find('h3').text();
+        selector.map(function (index , el) {
+            index++;
+            let title = $(el).attr('data-title-fa');
+            let href = $(el).find('a.js-product-url').attr('href');
+            let img = $(el).find('img').attr('src');
 
-        obj = {
-            "title" : title,
-            "url" : href
-        };
-        arr.push(obj);
-    });
+            const metadata = {
+                "title" : title,
+                "url" : href,
+                "image" : img,
+                "count" : index
+            };
+            parsedResults.push(metadata);
+        });
 
 
+        // Pagination Elements Link
+        const nextPageLink = 'https://www.digikala.com' + $('.c-pager__items').find('a.c-pager__item.is-active').parent().next().find('a').attr('href');
+
+        console.log(chalk.cyan(`Scraping: ${nextPageLink}`));
+        pageCounter++;
+        console.log(pageCounter);
+
+        if (pageCounter === pageLimit) {
+            exportResults(parsedResults);
+            return false;
+        }
+        await fetchData(nextPageLink);
+
+    } catch (error) {
+        exportResults(parsedResults);
+        console.error(error)
+    }
+};
+
+const exportResults = (parsedResults) => {
     let values = [];
-    for(var i=0; i< arr.length; i++){
-        values.push([arr[i].title,arr[i].url]);
+    for(let i=0; i< parsedResults.length; i++){
+        values.push([parsedResults[i].title,parsedResults[i].url,parsedResults[i].image,cat_id]);
     }
     //Bulk insert using nested array [ [a,b],[c,d] ] will be flattened to (a,b),(c,d)
-    connection.query(`INSERT INTO ${config.tables.ProductsTable} (title, url) VALUES ?`, [values], function(err,result) {
+    connection.query(`INSERT INTO ${config.tables.ProductsTable} (title, url , image, category_id) VALUES ?`, [values], function(err,result) {
         if (err) {
             console.log(err);
         } else {
-            console.log('Success');
+            console.log('All Is Done');
         }
     });
+};
 
-});
-
-async function fetchData(url){
-    console.log("Crawling data...");
-    // make http call to url
-    let response = await axios(url).catch((err) => console.log(err));
-
-    if(response.status !== 200){
-        console.log("Error occurred while fetching data");
-        return;
-    }
-    return response;
-}
+fetchData(url);
