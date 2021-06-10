dd99926c2fa4486ec9aad3a1b2be352787b27553
ali <wizard2070@gmail.com>
2021-06-10 21:11:28 +0430

    Added categories and cmdargs

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const mysql = require('mysql');
 const chalk = require('chalk');
+const cmdArgs = require('yargs').argv;
 const config = require('./config');
-const url = "https://www.digikala.com/search/category-mobile-phone";
-const catId = 1;
+
 
 const connection = mysql.createConnection({
     host: config.db.fullstack_express.host,
@@ -14,22 +14,43 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
+// Get URL
+const catId = cmdArgs['cat-id'];
+connection.query(`SELECT url FROM ${config.tables.CategoriesTable} WHERE id=${catId} `,
+    function(err,row,fields){
+        if (err) fetchData('Error');
+        fetchData(row);
+    });
+
+// Variables
 let pageLimit = 3;
 let pageCounter = 0;
 let parsedResults = [];
 let start_crawl_time = '';
-console.log(chalk.yellow.bgBlue(`\n  Scraping of ${chalk.underline.bold(url)} initiated...\n`))
+let products_count = 0;
 
-const fetchData = async (url) => {
+
+const fetchData = async (row) => {
+
+    if (row.length === 0){
+        console.log(chalk.red.bgBlack("No Such Category!!!"));
+        process.exit();
+        return;
+    }
+
+    let url = '';
+    (row instanceof Object) ? url = await row[0].url : url=row;
+    console.log(chalk.yellow.bgBlue(`\n  Scraping of ${chalk.underline.bold(url)} initiated...\n`));
+
     start_crawl_time = getCurrentDate();
-
     try {
         const html = await axios.get(url);
         const $ = cheerio.load(html.data);
         const selector = $('ul.c-listing__items.js-plp-products-list > li > .c-product-box');
 
         selector.map(function (index , el) {
-            index++;
+            //index++;
+            products_count++;
             let title = $(el).attr('data-title-fa');
             let href = $(el).find('a.js-product-url').attr('href');
             let img = $(el).find('img').attr('src');
@@ -37,12 +58,10 @@ const fetchData = async (url) => {
             const metadata = {
                 "title" : title,
                 "url" : href,
-                "image" : img,
-                "count" : index
+                "image" : img
             };
             parsedResults.push(metadata);
         });
-
 
         // Pagination Elements Link
         const nextPageLink = 'https://www.digikala.com' + $('.c-pager__items').find('a.c-pager__item.is-active').parent().next().find('a').attr('href');
@@ -59,8 +78,8 @@ const fetchData = async (url) => {
         await fetchData(nextPageLink);
 
     } catch (error) {
-        //exportResults(parsedResults , catId);
-        console.error(error)
+        console.error(error);
+        process.exit();
     }
 };
 
@@ -75,27 +94,21 @@ const exportResults = (parsed_results,cat_id,sct) => {
     connection.query(`INSERT INTO ${config.tables.ProductsTable} (title, url , image, category_id,date) VALUES ?`,
         [values],
         function(err,result) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('All Is Done');
+            if (err) console.log(err);
 
+            console.log('All Is Done');
             connection.query(`DELETE FROM ${config.tables.ProductsTable} WHERE category_id=${cat_id} AND date<${String(ect)} `,
                 function(err,resp){
                     if (err) throw err;
                     console.log('All Old Data Is Deleted.');
                 });
-        }
-    });
+        });
 
-    connection.query(`INSERT INTO ${config.tables.LogsTable} (start_crawl_time, end_crawl_time) VALUES (${sct},${ect})`,
+    connection.query(`INSERT INTO ${config.tables.LogsTable} (start_crawl_time, end_crawl_time , count , category_id) VALUES (${sct},${ect},${products_count},${cat_id})`,
         function(err,result) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('Logs Added.');
-            }
-    });
+            if (err)  console.log(err);
+            console.log('Logs Added.');
+        });
 };
 
 function getCurrentDate(){
@@ -103,4 +116,3 @@ function getCurrentDate(){
     return currentDate.getTime();
 }
 
-fetchData(url);
