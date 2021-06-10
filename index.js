79e1c616a30a3afeaa330deb99d8a08be93e1d7d
ali <wizard2070@gmail.com>
2021-06-10 20:49:56 +0430

    Added logs

--- index.js
+++ index.js
@@ -17,9 +17,12 @@ connection.connect();
 let pageLimit = 3;
 let pageCounter = 0;
 let parsedResults = [];
+let start_crawl_time = '';
 console.log(chalk.yellow.bgBlue(`\n  Scraping of ${chalk.underline.bold(url)} initiated...\n`))
 
 const fetchData = async (url) => {
+    start_crawl_time = getCurrentDate();
+
     try {
         const html = await axios.get(url);
         const $ = cheerio.load(html.data);
@@ -49,7 +52,7 @@ const fetchData = async (url) => {
         console.log(pageCounter);
 
         if (pageCounter === pageLimit) {
-            exportResults(parsedResults , catId);
+            exportResults(parsedResults , catId , start_crawl_time);
             return false;
         }
 
@@ -61,28 +64,43 @@ const fetchData = async (url) => {
     }
 };
 
-const exportResults = (parsed_results,cat_id) => {
+const exportResults = (parsed_results,cat_id,sct) => {
     let values = [];
-    const currentDate = new Date();
-    const timestamp = currentDate.getTime();
+    const ect = getCurrentDate();
 
     for(let i=0; i< parsedResults.length; i++){
-        values.push([parsed_results[i].title,parsed_results[i].url,parsed_results[i].image,cat_id,String(timestamp)]);
+        values.push([parsed_results[i].title,parsed_results[i].url,parsed_results[i].image,cat_id,String(ect)]);
     }
     //Bulk insert using nested array [ [a,b],[c,d] ] will be flattened to (a,b),(c,d)
-    connection.query(`INSERT INTO ${config.tables.ProductsTable} (title, url , image, category_id,date) VALUES ?`, [values], function(err,result) {
+    connection.query(`INSERT INTO ${config.tables.ProductsTable} (title, url , image, category_id,date) VALUES ?`,
+        [values],
+        function(err,result) {
         if (err) {
             console.log(err);
         } else {
             console.log('All Is Done');
 
-            connection.query(`DELETE FROM ${config.tables.ProductsTable} WHERE category_id=${cat_id} AND date<${String(timestamp)} `,
+            connection.query(`DELETE FROM ${config.tables.ProductsTable} WHERE category_id=${cat_id} AND date<${String(ect)} `,
                 function(err,resp){
                     if (err) throw err;
                     console.log('All Old Data Is Deleted.');
                 });
         }
     });
+
+    connection.query(`INSERT INTO ${config.tables.LogsTable} (start_crawl_time, end_crawl_time) VALUES (${sct},${ect})`,
+        function(err,result) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Logs Added.');
+            }
+    });
 };
+
+function getCurrentDate(){
+    const currentDate = new Date();
+    return currentDate.getTime();
+}
 
 fetchData(url);
